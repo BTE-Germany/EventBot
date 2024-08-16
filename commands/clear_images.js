cf50925fea0209b89b857636d8a1600c1135bb83
Michael R. <hi@mic-r.eu>
2024-08-16 19:56:36 +0000

    Fix missing require

--- commands/clear_images.js
+++ commands/clear_images.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const { BlobServiceClient } = require("@azure/storage-blob");
 const blobServiceClient = BlobServiceClient.fromConnectionString(
   process.env.AZURE_STORAGE_CONNECTION_STRING
 );
