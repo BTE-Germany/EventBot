dd29892a2eebc0ce3ba8ddde11628b3afc3caa33
Michael R. <hi@mic-r.eu>
2025-02-05 21:35:23 +0000

    Refactor updateStats to improve error handling and streamline build message processing

--- schedule/updateStats.js
+++ schedule/updateStats.js
@@ -1,45 +1,56 @@
 module.exports = {
-  time: 3000,
-  run: async (client, prisma) => {
-     // Disable this schedule
-    console.log(new Date().toLocaleString(), "Stats werden geupdated...");
-    // Get all users from the database
-    let users = await prisma.user.findMany();
-    let builds = await prisma.build.findMany();
-    //get every build message and extract the date of creation and then add it to the builds array
-    try {
-      builds = await builds.map(async (build) => {
-        let buildMessage = await client.channels.cache
-          .get(process.env.SUBMISSION_CHANNEL)
-          .messages.fetch(build.message);
-        build["date"] = buildMessage.createdTimestamp;
-        return build;
-      });
-
-      console.log(new Date().toLocaleString(), "Stats - Builds wurden geladen...");
-
-      //sort the builds by date
-      builds = await Promise.all(builds);
-      builds = builds.sort((a, b) => a.date - b.date);
-
-      //send to webhook
-      fetch(process.env.WEBHOOK_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          builds: builds,
-          users: users,
-        }),
-      }).then((res) => {
-        console.log(new Date().toLocaleString(), "Stats - Webhook wurde gesendet...");
-      });
-      
-    } catch (error) {
-      console.log(error);
-    }
-
-    console.log(new Date().toLocaleString(), "Stats wurden geupdated...");
-  },
-};
+    time: 3000,
+    run: async (client, prisma) => {
+      // Disable this schedule
+      console.log(new Date().toLocaleString(), "Stats werden geupdated...");
+      try {
+        // Get all users from the database
+        let users = await prisma.user.findMany();
+        let builds = await prisma.build.findMany();
+  
+        // Get every build message and extract the date of creation and then add it to the builds array
+        builds = await Promise.all(
+          builds.map(async (build) => {
+            let buildMessage = await client.channels.cache
+              .get(process.env.SUBMISSION_CHANNEL)
+              .messages.fetch(build.message);
+            build["date"] = buildMessage.createdTimestamp;
+            return build;
+          })
+        );
+  
+        console.log(
+          new Date().toLocaleString(),
+          "Stats - Builds wurden geladen..."
+        );
+  
+        // Sort the builds by date
+        builds = builds.sort((a, b) => a.date - b.date);
+  
+        // Send to webhook
+        await fetch(process.env.WEBHOOK_URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            builds: builds,
+            users: users,
+          }),
+        });
+  
+        console.log(
+          new Date().toLocaleString(),
+          "Stats - Webhook wurde gesendet..."
+        );
+      } catch (error) {
+        console.error(
+          new Date().toLocaleString(),
+          "Stats - Fehler beim Senden des Webhooks:",
+          error
+        );
+      }
+  
+      console.log(new Date().toLocaleString(), "Stats wurden geupdated...");
+    },
+  };
