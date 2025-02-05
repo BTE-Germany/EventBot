1fd99b9a892d13870a358061842d66262836b854
Michael R. <hi@mic-r.eu>
2025-02-05 21:39:50 +0000

    Refactor updateStats to improve build message fetching and streamline data processing

--- schedule/updateStats.js
+++ schedule/updateStats.js
@@ -1,56 +1,59 @@
 module.exports = {
-    time: 3000,
-    run: async (client, prisma) => {
-      // Disable this schedule
-      console.log(new Date().toLocaleString(), "Stats werden geupdated...");
-      try {
-        // Get all users from the database
-        let users = await prisma.user.findMany();
-        let builds = await prisma.build.findMany();
-  
-        // Get every build message and extract the date of creation and then add it to the builds array
-        builds = await Promise.all(
-          builds.map(async (build) => {
-            let buildMessage = await client.channels.cache
-              .get(process.env.SUBMISSION_CHANNEL)
-              .messages.fetch(build.message);
-            build["date"] = buildMessage.createdTimestamp;
-            return build;
-          })
-        );
-  
-        console.log(
-          new Date().toLocaleString(),
-          "Stats - Builds wurden geladen..."
-        );
-  
-        // Sort the builds by date
-        builds = builds.sort((a, b) => a.date - b.date);
-  
-        // Send to webhook
-        await fetch(process.env.WEBHOOK_URL, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            builds: builds,
-            users: users,
-          }),
-        });
-  
-        console.log(
-          new Date().toLocaleString(),
-          "Stats - Webhook wurde gesendet..."
-        );
-      } catch (error) {
-        console.error(
-          new Date().toLocaleString(),
-          "Stats - Fehler beim Senden des Webhooks:",
-          error
-        );
-      }
-  
-      console.log(new Date().toLocaleString(), "Stats wurden geupdated...");
-    },
-  };
+  time: 3000,
+  run: async (client, prisma) => {
+    // Disable this schedule
+    console.log(new Date().toLocaleString(), "Stats werden geupdated...");
+    try {
+      // Get all users from the database
+      let users = await prisma.user.findMany();
+      let builds = await prisma.build.findMany();
+
+      // Get every build message and extract the date of creation and then add it to the builds array
+      let buildMessages = await client.channels.cache
+        .get(process.env.BUILD_CHANNEL)
+        .messages.fetch({ limit: 250 });
+
+      builds = builds.map((build) => {
+        let message = buildMessages.get(build.message_id);
+        if (!message) return;
+        return {
+          date: message.createdTimestamp,
+          user: build.user_id,
+        };
+      });
+
+      console.log(
+        new Date().toLocaleString(),
+        "Stats - Builds wurden geladen..."
+      );
+
+      // Sort the builds by date
+      builds = builds.sort((a, b) => a.date - b.date);
+
+      // Send to webhook
+      await fetch(process.env.WEBHOOK_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          builds: builds,
+          users: users,
+        }),
+      });
+
+      console.log(
+        new Date().toLocaleString(),
+        "Stats - Webhook wurde gesendet..."
+      );
+    } catch (error) {
+      console.error(
+        new Date().toLocaleString(),
+        "Stats - Fehler beim Senden des Webhooks:",
+        error
+      );
+    }
+
+    console.log(new Date().toLocaleString(), "Stats wurden geupdated...");
+  },
+};
