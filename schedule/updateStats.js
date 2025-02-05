c060a17a516bd933668343c36444d013e4606e25
Michael R. <hi@mic-r.eu>
2025-02-05 21:23:23 +0000

    Disable stats update schedule temporarily

--- schedule/updateStats.js
+++ schedule/updateStats.js
@@ -1,6 +1,7 @@
 module.exports = {
   time: 3000,
   run: async (client, prisma) => {
+    return; // Disable this schedule
     console.log(new Date().toLocaleString(), "Stats werden geupdated...");
     // Get all users from the database
     let users = await prisma.user.findMany();
