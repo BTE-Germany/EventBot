d63561971838e23d587f9c478fe53b5e0cbacf41
Michael R. <hi@mic-r.eu>
2025-02-09 17:34:39 +0000

    Refactor info_ modal to improve message editing and user feedback

--- modals/info_.js
+++ modals/info_.js
@@ -1,52 +1,61 @@
 module.exports = {
-    modal: {
-        name: "info_"
-    },
-    run: async (client, interaction, prisma) => {
-        const judge_msg = await prisma.build.findUnique({
-            where: {
-                id: parseInt(interaction.customId.split("_")[1])
-            }
-        });
+  modal: {
+    name: "info_",
+  },
+  run: async (client, interaction, prisma) => {
+    const judge_msg = await prisma.build.findUnique({
+      where: {
+        id: parseInt(interaction.customId.split("_")[1]),
+      },
+    });
 
-        //get the message
-        const message = await client.channels.cache.get(process.env.JUDGE_CHANNEL).messages.fetch(judge_msg.judge_msg.toString());
-        let newMessage = message;
+    //get the message
+    const message = await client.channels.cache
+      .get(process.env.JUDGE_CHANNEL)
+      .messages.fetch(judge_msg.judge_msg.toString());
+    let newMessage = message;
 
-        const possible_Inputs = ["3d_view", "street_view", "street_view_link", "other_info"];
-        const value = possible_Inputs.map((v) => {
-            const value = interaction.fields.getTextInputValue(v);
-            if(!value || value === "") return;
-            switch (v) {
-                case "3d_view":
-                    return `3D-View benutzt: ${value}`;
-                case "street_view":
-                    return `Street-View benutzt: ${value}`;
-                case "street_view_link":
-                    return `Link zu Street-View: ${value}`;
-                case "other_info":
-                    return `Sonstige Informationen: ${value}`;
-            }
-        }).filter((v) => v !== undefined).join("\n");
+    const possible_Inputs = [
+      "3d_view",
+      "street_view",
+      "street_view_link",
+      "other_info",
+    ];
+    const value = possible_Inputs
+      .map((v) => {
+        const value = interaction.fields.getTextInputValue(v);
+        if (!value || value === "") return;
+        switch (v) {
+          case "3d_view":
+            return `3D-View benutzt: ${value}`;
+          case "street_view":
+            return `Street-View benutzt: ${value}`;
+          case "street_view_link":
+            return `Link zu Street-View: ${value}`;
+          case "other_info":
+            return `Sonstige Informationen: ${value}`;
+        }
+      })
+      .filter((v) => v !== undefined)
+      .join("\n");
 
-        newMessage.embeds[0].fields.push({
-            name: "Zusätzliche Informationen",
-            value: value
-        });
+    newMessage.embeds[0].fields.push({
+      name: "Zusätzliche Informationen",
+      value: value,
+    });
 
-        //edit the message
-        message.edit(
-            {
-                content: newMessage.content,
-                embeds: newMessage.embeds
-            }
-        )
+    console.log(newMessage.embeds[0].fields);
 
-        //reply to the user
-        await interaction.reply({
-            content: "Zusätzliche Informationen hinzugefügt.",
-            ephemeral: true
-        });
+    //edit the message
+    await message.edit({
+      content: newMessage.content,
+      embeds: newMessage.embeds,
+    });
 
-    }
-}
+    //reply to the user
+    await interaction.reply({
+      content: "Zusätzliche Informationen hinzugefügt.",
+      ephemeral: true,
+    });
+  },
+};
