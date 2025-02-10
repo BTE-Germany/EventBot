0a1f04827c175e2104425aa426a66a9b76e9ae8a
Michael R. <hi@mic-r.eu>
2025-02-10 12:57:49 +0000

    Add unjudged command to retrieve unjudged builds from the database

---
+++ commands/unjudged.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+module.exports = {
+  command: {
+    name: "unjudged",
+    description: "Ruft alle unjudizierten Builds ab.",
+  },
+  run: async (client, interaction, prisma) => {
+    //get all unjudged builds, that means judges[] cardinality is < 2
+    const unjudgedBuilds = await prisma.build.$queryRaw`SELECT id, \'https://discord.com/channels/${process.env.GUILD_ID}/${process.env.JUDGE_CHANNEL}/\' || judge_msg AS discord_url FROM objects WHERE cardinality(judges) < 2;`;
+
+    if (unjudgedBuilds.length === 0) {
+      return interaction.reply({
+        content: "Alle Builds wurden bereits bewertet.",
+        ephemeral: true,
+      });
+    }
+
+    const unjudgedBuildsLong = unjudgedBuilds.map((build) => {
+        return `[#${build.id}](${build.discord_url})`;
+    }).join("\n");
+
+    if(unjudgedBuildsLong.length > 2000) {
+        const unjudgedBuildsShort = unjudgedBuilds.map((build) => {
+            return `#${build.id}`;
+        }).join("\n");
+        return interaction.reply({
+            content: `Die Liste ist zu lang, um sie anzuzeigen. Hier sind die IDs:\n${unjudgedBuildsShort}`,
+            ephemeral: true,
+        });
+    }
+
+    return interaction.reply({
+      content: `Folgende Builds wurden noch nicht bewertet:\n${unjudgedBuildsLong}`,
+      ephemeral: true,
+    });
+  }
+};
